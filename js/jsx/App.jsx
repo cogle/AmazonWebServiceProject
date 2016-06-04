9eb9caf90782164b43df3a66e915a8fac8d6dbcc
Christopher Ogle <thecogle@gmail.com>
2016-06-04 12:40:57 -0500

    Reformating Site to Be Genereal Amazon Services Viewer

--- js/jsx/App.jsx
+++ js/jsx/App.jsx
@@ -1,36 +1,11 @@
 import React from 'react';
-import BucketList from './BucketList.jsx';
-import BucketInfo from './BucketInfo.jsx';
+import BucketExplorer import './BucketExplorer.jsx';
 
-export default class App extends React.Component {
-   constructor(props){
-      super(props);
-      this.state = {curChoice : 0,
-                    curBucket: ''};
-   }
-   switchChoice(newChoice){
-      this.setState({curChoice : newChoice});
-   }
-   setBucketName(name){
-      this.setState({curBucket : name});
-   }
-   render() {
-      let content;
-      if(this.state.curChoice === 0){
-         content = ( <div><h3>Amazon Bucket Lookup</h3>
-                     <BucketList switchFunc={this.switchChoice.bind(this)}
-                     setBucket={this.setBucketName.bind(this)}/></div>);
-      }
-      else{
-         content = ( <div><h3>Display information for {this.state.curBucket}</h3>
-                     <BucketInfo switchFunc={this.switchChoice.bind(this)}
-                                 curBucket={this.state.curBucket} 
-                                 pollInterval={1000}/></div>);
-      }
+class App extends React.Component {
+   render() { 
       return (
-         <div className="container center-align">
+         <div>
 
-            {content}
          </div>
       );
    }
---
+++ js/jsx/BucketExplorer.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import BucketList from './BucketList.jsx';
+import BucketInfo from './BucketInfo.jsx';
+
+export default class BucketExplorer extends React.Component {
+   constructor(props){
+      super(props);
+      this.state = {curChoice : 0,
+                    curBucket: ''};
+   }
+   switchChoice(newChoice){
+      this.setState({curChoice : newChoice});
+   }
+   setBucketName(name){
+      this.setState({curBucket : name});
+   }
+   render() {
+      let content;
+      if(this.state.curChoice === 0){
+         content = ( <div><h3>Amazon Bucket Lookup</h3>
+                     <BucketList switchFunc={this.switchChoice.bind(this)}
+                     setBucket={this.setBucketName.bind(this)}/></div>);
+      }
+      else{
+         content = ( <div><h3>Display information for {this.state.curBucket}</h3>
+                     <BucketInfo switchFunc={this.switchChoice.bind(this)}
+                                 curBucket={this.state.curBucket}
+                                 pollInterval={1000}/></div>);
+      }
+      return (
+         <div className="container center-align">
+            {content}
+         </div>
+      );
+   }
+}
